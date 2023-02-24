8c3101b959e28175a2562797b8a050bac11e9126
yorumimi <yorumimi18@gmail.com>
2023-02-24 11:32:17 +0900

    create window

--- quince/quince/application/win32/GameWindow.cpp
+++ quince/quince/application/win32/GameWindow.cpp
@@ -1,13 +1,28 @@
 #include "GameWindow.h"
 
 namespace quince {
-
+  
 GameWindow::GameWindow()
 {
+  auto window = SDL_CreateWindow("test window", 100, 100, 1024, 768, SDL_WINDOW_OPENGL);
+  
+  mWindow = std::make_unique<SDL_Window*>(window);
+
+  if (!mWindow)
+  {
+    SDL_Log("Failed to create window: %s", SDL_GetError());
+  }
 }
 
 GameWindow::~GameWindow()
 {
+  SDL_DestroyWindow(*mWindow.get());
+  mWindow.reset();
+}
+
+SDL_Window* GameWindow::GetWindowPointer()
+{
+  return *mWindow.get();
 }
 
 }  // namespace quince
--- quince/quince/application/win32/GameWindow.h
+++ quince/quince/application/win32/GameWindow.h
@@ -1,5 +1,10 @@
 #pragma once
 
+//NOTE: ヘッダファイル大きくなるからSDL.hをincludeしないといけないファイルは1つにまとめてもよい？
+#include "build/windows/_deps/sdl2-src/include/SDL.h"
+
+#include <memory>
+
 namespace quince {
 
 class GameWindow final
@@ -8,6 +13,12 @@ public:
   GameWindow();
 
   ~GameWindow();
+
+  [[nodiscard]] SDL_Window* GetWindowPointer();
+
+private:
+  //NOTE: SDL_Window structにポインタが含まれているため
+  std::unique_ptr<SDL_Window*> mWindow;
 };
 
 }  // namespace quince
